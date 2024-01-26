d61b2a984329f46710ba7bbf8c90e6dbc0081cdf
sadeghii <sadeghii@hasin.ir>
2024-01-26 14:15:19 +0330

    feat(exception): CustomErrorException has been added

---
+++ src/main/java/com/TOSAN/onlineBookStore/exception/CustomErrorException.java
@@ -0,0 +1,20 @@
+package com.TOSAN.onlineBookStore.exception;
+
+public class CustomErrorException extends Exception{
+
+    public CustomErrorException() {
+        super();
+    }
+
+    public CustomErrorException(Throwable e) {
+        super(e);
+    }
+
+    public CustomErrorException(String message) {
+        super(message);
+    }
+
+    public CustomErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
