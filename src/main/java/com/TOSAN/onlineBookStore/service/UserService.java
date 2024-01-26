1699cb4870327cb62fb4abc67ad623e299b5ed53
sadeghii <sadeghii@hasin.ir>
2024-01-26 14:04:36 +0330

    custom service has been created

---
+++ src/main/java/com/TOSAN/onlineBookStore/service/UserService.java
@@ -0,0 +1,41 @@
+package com.TOSAN.onlineBookStore.service;
+
+import com.TOSAN.onlineBookStore.model.Customer;
+import com.TOSAN.onlineBookStore.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<Customer> customer = customerRepository.findByUsername(username);
+
+        // Converting userDetail to UserDetails
+        return customer.map(CustomerDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + username));
+    }
+
+    public String addUser(Customer userInfo) {
+        userInfo.setPassword(encoder.encode(userInfo.getPassword()));
+        customerRepository.save(userInfo);
+        return "User Added Successfully";
+    }
+
+
+}
